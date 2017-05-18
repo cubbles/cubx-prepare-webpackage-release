3624442c66dd4c376d4efcc4c6b734b9f95fce44
Edwin Gamboa <edwjov92@gmail.com>
2017-05-18 10:40:52 -0500

    CDT-52: Add bin script

---
+++ bin/cubx-prepare-webpackage-release.js
@@ -0,0 +1,30 @@
+'use strict';
+var WebpackageReleasePreparer = require('../lib/cubx-prepare-webpackage-release');
+var commandLineArgs = require('command-line-args');
+
+var webpackagePath;
+
+var optionDefinitions = [
+  { name: 'webpackagePath', type: String, defaultOption: true, alias: 'p' },
+  { name: 'releaseVersion', type: String, alias: 'r' },
+  { name: 'nextVersion', type: String, alias: 'n' }
+];
+
+var options = commandLineArgs(optionDefinitions);
+var versions = {};
+
+if (!options.webpackagePath) {
+  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-prepare-webpackage-release -p <webpackagPath>');
+  process.exit(0);
+} else {
+  webpackagePath = options.webpackagePath;
+}
+
+if (options.releaseVersion) {
+  versions.releaseVersion = options.releaseVersion;
+}
+if (options.nextVersion) {
+  versions.nextVersion = options.nextVersion;
+}
+var preparer = new WebpackageReleasePreparer(webpackagePath, versions);
+preparer.prepareUpload();
